eb725b00ba20a36fab70ce862a8cdf207668de78
Arsym <123092893+Arsym-dev@users.noreply.github.com>
2023-01-22 07:01:16 -0500

    Made it easier to save the token

--- README.md
+++ README.md
@@ -4,13 +4,39 @@ Export your betting history from [WinnerOdds](https://winnerodds.com/) to a CSV 
 
 Precompiled binaries can be found in [Releases](https://github.com/arsym-dev/WinnerOddsExport/releases).
 
-## Usage
-
-Get your token (see below), then run the exporter to obtain your CSV file. It may take a few minutes if you have a large number of bets.
-
 ![Main UI](img/main.png)
 ![Betting data](img/bet_history.png)
 
+## Usage
+
+1. Run the GUI
+2. Get your token (see below) and paste it in the field
+3. Set the start date, end date, and sport
+4. Press "Download CSV"
+
+It may take a few minutes if you have a large number of bets.
+
+## Running from source
+
+1. Make sure you have [Python3](https://www.python.org/downloads/) installed
+2. Open a terminal and install all the dependencies:
+
+```bash
+pip3 install request python-dateutil tk
+```
+
+3. In the terminal, clone this repository and navigate to it:
+
+```bash
+git clone https://github.com/arsym-dev/WinnerOddsExport.git
+cd WinnerOddsExport
+```
+
+4. Finally, run the GUI:
+
+```bash
+python3 gui.py
+```
 
 ## Getting your token
 
@@ -19,9 +45,9 @@ To get your WinnerOdds authorization token, do the following:
 1. Log in to [https://app.winnerodds.com](https://app.winnerodds.com)
 2. Open the broswer inspector. You can do this by right clicking the page and pressing "Inspect".
 3. In the inspector, go to the "Network" tab.
-4. On the WinnerOdds website, change the page to the odds page or tracking page. You should now see a new entry in the Network tab that's calling `https://app.winnerodds.com:4000/graphql`
-5. Click this entry, scroll down to "Request Headers" and find the field called "authorization". It should have a value of `Bearer <token>`
-6. Copy the value of `<token>` and save it into "token.txt". **DO NOT SHARE YOUR TOKEN WITH ANYONE**. If you do, other people may log into your account.
+4. Refresh the WinnerOdds website. In the Network tab, you should see some entries that have the URL: `https://app.winnerodds.com:4000/graphql`
+5. Click one of these entries, scroll down to "Request Headers" and find the field called "authorization". It should have a value of `Bearer <token>`. Copy this value and paste it into the "Token" field.
+
+**DO NOT SHARE YOUR TOKEN WITH ANYONE**. If you do, other people may log into your account.
 
 ![Main UI](img/token.png)
-
--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ import threading
 
 from winnerodds_export_csv import *
 
-
+token_fname = "token.txt"
 datetime_format = "%Y-%m-%d"
 
 class App(tk.Frame):
@@ -19,34 +19,45 @@ class App(tk.Frame):
 
         padding = {"padx": 5, "pady": 5}
 
+        if os.path.exists("token.txt"):
+            with open("token.txt") as f:
+                token = f.read()
+        else:
+            token = ""
+
         window.title("WinnerOdds Bet History Exporter")
-        window.geometry('300x180')
+        window.geometry('300x200')
         window.grid_columnconfigure(0, weight=1)
         window.grid_columnconfigure(1, weight=1)
 
-        Label(window, text="Start Date").grid(column=0, row=0)
-        Label(window, text="End Date").grid(column=0, row=1)
-        Label(window, text="Sport").grid(column=0, row=2)
-        Label(window, text="").grid(column=0, row=3)
+        Label(window, text="Token").grid(column=0, row=0)
+        Label(window, text="Start Date").grid(column=0, row=1)
+        Label(window, text="End Date").grid(column=0, row=2)
+        Label(window, text="Sport").grid(column=0, row=3)
+        Label(window, text="").grid(column=0, row=4)
+
+        self.tokField = Entry(window)
+        self.tokField.grid(column=1, row=0, **padding)
+        self.tokField.insert(0, token)
 
         self.startdate = Entry(window)
-        self.startdate.grid(column=1, row=0, **padding)
+        self.startdate.grid(column=1, row=1, **padding)
         self.startdate.insert(0, "1970-12-31")
 
         self.enddate = Entry(window)
-        self.enddate.grid(column=1, row=1, **padding)
+        self.enddate.grid(column=1, row=2, **padding)
         self.enddate.insert(0, datetime.now().strftime(datetime_format))
 
         self.sportVar = StringVar()
         self.sportVar.set("TENNIS")
         sport = OptionMenu(window, self.sportVar, "TENNIS", "FOOTBALL")
-        sport.grid(column=1, row=2, **padding)
+        sport.grid(column=1, row=3, **padding)
 
         btn = Button(window, text="Download CSV", command=self.clicked)
-        btn.grid(column=0, row=4, columnspan=2)
+        btn.grid(column=0, row=5, columnspan=2)
 
         self.progress = Label(window, text="")
-        self.progress.grid(column=0, row=5, columnspan=2)
+        self.progress.grid(column=0, row=6, columnspan=2)
     
 
     def clicked(self):
@@ -63,8 +74,17 @@ class App(tk.Frame):
         except:
             tk.messagebox.showerror(title="Error", message="Invalid end date. Must be YYYY-MM-DD")
             return
+        
+        ## Clean the token and save to file
+        token = self.tokField.get()
+        if token.startswith("Bearer "):
+            token = token[len("Bearer "):]
+        token = token.strip()
 
+        with open("token.txt", "w") as f:
+            f.write(token)
 
+        ## Start downloading CSV
         self.download_thread = threading.Thread(target=self.run)
         self.download_thread.daemon = True
         self.download_thread.start()
